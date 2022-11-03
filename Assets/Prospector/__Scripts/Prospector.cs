abcdad823dbca87078e25a01cb78072d8abfade2
FinnianBurns <fwb13@scarletmail.rutgers.edu>
2022-11-03 19:19:47 -0400

    Setting Up Layers Part 1

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -12,11 +12,18 @@ public class Prospector : MonoBehaviour {
 	[Header("Set in Inspector")]
 	public TextAsset deckXML;
 	public TextAsset layoutXML;
+	public float xOffset = 3;
+	public float yOffset = -2.5f;
+	public Vector3 layoutCenter;
 
 	[Header("Set Dynamically")]
 	public Deck	deck;
 	public Layout layout;
 	public List<CardProspector> drawPile;
+	public Transform layoutAnchor;
+	public CardProspector target;
+	public List<CardProspector> tableau;
+	public List<CardProspector> discardPile;
 
 	void Awake(){
 		S = this;
@@ -27,15 +34,17 @@ public class Prospector : MonoBehaviour {
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle(ref deck.cards);
 
+		drawPile = ConvertListCardsToListCardProspectors( deck.cards );
+		LayoutGame();
+
+	//Card c;
+	//for (int cNum=0; cNum<deck.cards.Count; cNum++) {
+		//c = deck.cards[cNum];
+		//c.transform.localPosition = new Vector3( (cNum%13)*3, cNum/13*4, 0 );
+	//}
+
 		layout = GetComponent<Layout>();
 		layout.ReadLayout(layoutXML.text);
-		drawPile = ConvertListCardsToListCardProspectors( deck.cards );
-
-	Card c;
-	for (int cNum=0; cNum<deck.cards.Count; cNum++) {
-		c = deck.cards[cNum];
-		c.transform.localPosition = new Vector3( (cNum%13)*3, cNum/13*4, 0 );
-	}
 	}
 
 	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card>lCD) {
@@ -46,6 +55,34 @@ public class Prospector : MonoBehaviour {
 			lCP.Add( tCP );
 		}
 		return( lCP );
+	}
+
+	CardProspector Draw() {
+		CardProspector cd = drawPile[0];
+		drawPile.RemoveAt(0);
+		return(cd);
+	}
+
+	void LayoutGame() {
+		if (layoutAnchor == null) {
+			GameObject tGO = new GameObject("_LayoutAnchor");
+			layoutAnchor = tGO.transform;
+			layoutAnchor.transform.position = layoutCenter;
+		}
+
+		CardProspector cp;
+		foreach (SlotDef tSD in layout.slotDefs) {
+			cp = Draw();
+			cp.faceUp = tSD.faceUp;
+			cp.transform.parent = layoutAnchor;
+			cp.transform.localPosition = new Vector3(layout.multiplier.x * tSD.x, layout.multiplier.y * tSD.y, -tSD.layerID);
+
+			cp.layoutID = tSD.id;
+			cp.slotDef = tSD;
+			cp.state = eCardState.tableau;
+
+			tableau.Add(cp);
+		}
 	}
 
 }
