d88b86006502bb22ffc16f2fe3c23c93ccc2c68f
FinnianBurns <fwb13@scarletmail.rutgers.edu>
2022-11-17 19:47:17 -0500

    ScoreManager

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -168,6 +168,7 @@ public class Prospector : MonoBehaviour {
 			MoveToDiscard(target);
 			MoveToTarget(Draw());
 			UpdateDrawPile();
+			ScoreManager.EVENT(eScoreEvent.draw);
 			break;
 
 			case eCardState.tableau:
@@ -183,6 +184,7 @@ public class Prospector : MonoBehaviour {
 			tableau.Remove(cd);
 			MoveToTarget(cd);
 			SetTableauFaces();
+			ScoreManager.EVENT(eScoreEvent.mine);
 			break;
 		}
 
@@ -210,9 +212,11 @@ public class Prospector : MonoBehaviour {
 
 	void GameOver(bool won) {
 		if (won) {
-			print ("Game Over. You won! :)");
+			//print ("Game Over. You won! :)");
+			ScoreManager.EVENT(eScoreEvent.gameWin);
 		} else {
-			print ("Game Over. You Lost. :(");
+			//print ("Game Over. You Lost. :(");
+			ScoreManager.EVENT(eScoreEvent.gameLoss);
 		}
 
 		SceneManager.LoadScene("__Prospector_Scene_0");
